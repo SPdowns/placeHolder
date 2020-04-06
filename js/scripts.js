bb86a5a6876fddff0d27d15f062bcf3ac22da997
Sean Downs <spdowns@protonmail.com>
2020-04-06 14:27:57 -0700

    adds a jquery append method that will also populate a no show class with the remaining data attached. Then a jQuery event listener .on listens for a click on an h2 element that then shows the hidden h3 elements.

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,18 @@
 //declaring a constructor with 2 keyvalue pair
 function PlacesIveBeen() {
   this.places = [];
-  this.currentId = 0
+  this.currentId = 0;
 }
 //addplaces is a protoype of the constructor PlacesIveBeen, and its a 
 //method of the object created with PlacesIveBeen
 PlacesIveBeen.prototype.addplaces = function(city) {
-  this.places.id = this.addID();
+  city.id = this.addID();
   this.places.push(city);
 }
-pla
+PlacesIveBeen.prototype.addID= function(){
+  this.currentId += 1;
+  return this.currentId;
+}
 //declaring a constructor with 4 keypair values
 function City(name, country, date, visits) {
   this.name = name;
@@ -45,11 +48,25 @@ $(document).ready(function(){
 //before running the next forLoop, we need to clear whats visible with the .empty functions
 //targeting the #returnValue div in the html
     $('#returnAnswer').empty();  
-//forEach loop runs through placesArr
+//forEach loop runs through placesArr 
     placesArr.forEach(function(city){
-     $('#returnAnswer').append(`<h2>${city.id}</h2>`);
-      
+     $('#returnAnswer').append(`<div>
+     <h2 id="${city.id}">${city.name}</h2>
+     <div id="city${city.id}info" class="no-show">
+     <h3>${city.country}</h3>
+     <h3>${city.date}</h3>
+     <h3>${city.visits}</h3>
+     </div>
+     </div>`); 
     })
-    
+   
+ 
   });
+
+   //attach event listener .on(event, target which is the h2, and action which in this case is showing a css class) for showing data tracks any click on a h2 that is inside <div id="returnAnswer">
+  $('#returnAnswer').on("click", 'h2', function(event){
+    //toggle no-show class on div that shrares number in its ID with h2 ID
+    $(`#city${event.target.id}info`).toggle('no-show')
+
+  })
 });
