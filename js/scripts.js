219b7fa96cdad86b3d64ec5704c9a69e43ae6968
Sean Downs <spdowns@protonmail.com>
2020-04-06 11:43:40 -0700

    adds front end logic and html

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,7 @@ PlacesIveBeen.prototype.addCities = function(city) {
 }
 
 
-function Cities(name, country, date, visits) {
+function City(name, country, date, visits) {
   this.name = name;
   this.country = country;
   this.date = date;
@@ -19,3 +19,25 @@ function Cities(name, country, date, visits) {
 }
 // var arr = [$('input1').val(), $('input2').val(), $('input3').val(),]
 // let place = new Cities('bahamas', 'Bahamas' , '04-3-2020', arr)
+
+// Front End
+
+
+$(document).ready(function(){
+  var randomArray = [{name: 'eric', age:24}, {name:'sean', age: 102}]
+  randomArray.forEach(person=>{
+    console.log(person.name)
+  })
+  var whateverFloats = new PlacesIveBeen();
+  $('#form-group').submit(function(event){
+    event.preventDefault();
+    var cityName = $('#cityName').val();
+    var countryName = $('#countryName').val();
+    var date = $('#date').val();
+    var placesVisited = $('placesVisited').val();
+
+    var wholePlace = new City(cityName, countryName, date, placesVisited)
+      whateverFloats.addCities(wholePlace)
+      
+  });
+});
